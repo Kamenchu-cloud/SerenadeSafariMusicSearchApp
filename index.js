bbbeeb68aa963e5310b5aa7b3e7f3f79446476f4
Kamenchu <iankamenchu@gmail.com>
2024-01-19 21:47:42 +0300

    initial js

---
+++ index.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const searchInput = document.getElementById("searchInput");
+    const searchBtn = document.getElementById("searchBtn");
+    const resultsList = document.getElementById("resultsList");
+    const audio = document.getElementById("myAudio");
+    const songName = document.getElementById("songName");
+    const songArtist = document.getElementById("songArtist");
+    const songAlbum = document.getElementById("songAlbum");
+  
+    const clientId = "28d7316839b3472dbd3c5fc2c32fe164";
+    const clientSecret = "ad3c13ad455647c2891e0e390e5550a3";
+    let accessToken;
+});
