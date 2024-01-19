62fa9bf67e6f3e0588997d89da3070f429516e8e
Kamenchu <iankamenchu@gmail.com>
2024-01-19 21:49:18 +0300

    improved js

--- index.js
+++ index.js
@@ -10,4 +10,17 @@ document.addEventListener("DOMContentLoaded", function () {
     const clientId = "28d7316839b3472dbd3c5fc2c32fe164";
     const clientSecret = "ad3c13ad455647c2891e0e390e5550a3";
     let accessToken;
+
+    function authenticateSpotify() {
+        const tokenUrl = "https://accounts.spotify.com/api/token";
+        const base64Credentials = btoa(`${clientId}:${clientSecret}`);
+        const tokenOptions = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Authorization: `Basic ${base64Credentials}`,
+          },
+          body: "grant_type=client_credentials",
+        };
+        
 });
